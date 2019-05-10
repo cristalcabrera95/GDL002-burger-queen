62eff9548e1a4073948169e47f67e988bf42fa4c
cristalcabrera95 <cristalcabrera9507@gmail.com>
2019-05-10 13:18:49 -0500

    Updates

---
+++ precache-manifest.01104668f6e81cfe67cb0d08be03ba70.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae69106d0ffaed315bfb6aa5e011a02b",
+    "url": "/GDL002-BURGER-QUEEN-cristal/index.html"
+  },
+  {
+    "revision": "f2b7dba4e3287af7a642",
+    "url": "/GDL002-BURGER-QUEEN-cristal/static/css/main.ca5b1310.chunk.css"
+  },
+  {
+    "revision": "87682c55fe178ee4e3cb",
+    "url": "/GDL002-BURGER-QUEEN-cristal/static/js/2.ff6fd18e.chunk.js"
+  },
+  {
+    "revision": "f2b7dba4e3287af7a642",
+    "url": "/GDL002-BURGER-QUEEN-cristal/static/js/main.26f6e6d7.chunk.js"
+  },
+  {
+    "revision": "ba9a50adaa23ac72d38b",
+    "url": "/GDL002-BURGER-QUEEN-cristal/static/js/runtime~main.16e83175.js"
+  }
+]);
